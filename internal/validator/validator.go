9be09f721df8c329ca338744b5395e3350e33151
Ken Muyesu <kmuyesu@gmail.com>
2025-06-20 06:13:32 +0300

    chore: user signup and password encryption

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -4,11 +4,15 @@ import (
 	"slices"
 	"strings"
 	"unicode/utf8"
+	"regexp"
 )
+
 
 type Validator struct {
 	FieldErrors map[string]string
 }
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
@@ -41,4 +45,12 @@ func MaxChars(value string, n int) bool {
 
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
